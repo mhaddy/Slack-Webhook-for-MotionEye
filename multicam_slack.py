c41050b733c37bc7ae4fd62f674f4161dc3adb14
mhaddy <mhaddy@gmail.com>
2019-02-24 17:34:02 -0500

    Added support for multiple cameras

--- multicam_slack.py
+++ multicam_slack.py
@@ -3,11 +3,13 @@
 
 # license: MIT
 
-# Single-cam Slack webhook for MotionEyeOS
-# No longer maintained - refer to multicam_slack.py for newest updates
+# Multi-cam Slack webhook for MotionEyeOS
 # adapted from https://github.com/IAmOrion/MotionEyeOS_Add-On_Scripts with inspiration from:
 # - https://github.com/ccrisan/motioneyeos/issues/1557
 # - https://github.com/kabadisha/motioneyeos-discord-notifier
+
+# USAGE
+# python /{absolute-path-to-your-motion-install}/data/Slack-Webhook-for-MotionEye/multicam_slack.py -n "Garage" -p %f -q %q -v %v &
 
 # Ryan Matthews
 # https://github.com/mhaddy
@@ -15,12 +17,15 @@
 
 import config as cv
 import pycurl, cStringIO, glob, os, json, pytz, datetime, time
-import logging
+import logging, argparse
 from tzlocal import get_localzone
+from argparse import RawDescriptionHelpFormatter
 
 global FILENAME, FILE_FOUND
 
-logging.basicConfig(filename=cv.LOG_DIR+cv.LOG_FILENAME,format='%(asctime)s : %(levelname)s : %(message)s',level=logging.DEBUG)
+# change logging.INFO to logging.DEBUG at the end of this line
+# if you're experiencing issues or need help during initial setup
+logging.basicConfig(filename=cv.LOG_DIR+cv.LOG_FILENAME,format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
 WEBHOOK_URL = "https://slack.com/api/files.upload"
 WEBHOOK_AUTH_BEAR = "Bearer " + cv.BEARER_TOKEN
@@ -48,11 +53,19 @@ assert local_now.replace(tzinfo=None) == now
 CURRENT_TIME = local_now.strftime('%Y-%m-%dT%H:%M:%S.000Z') # UTC Formatted for use in Slack's timestamp argument
 MESSAGE_TIME = local_now.strftime(cv.MESSAGE_DATEFORMAT)
 
-def find_latest_file():
+# get details of the camera, path, and others from the call 
+parser = argparse.ArgumentParser(description="Multi-cam Slack notification webhook for MotionEye",epilog="Written by Mhaddy, inspired by IAmOrion and Kabadisha, https://www.github.com/mhaddy", formatter_class=RawDescriptionHelpFormatter)
+parser.add_argument("-n", "--cameraname", help="Camera name (something short and description, like Porch)", type=str, default="Camera")
+parser.add_argument("-p", "--mediadir", help="Absolute path to the media directory (with trailing slash)", type=str, default="/home/pi/motioneye/media/Camera1/")
+parser.add_argument("-q", "--frame", help="%%q option from MotionEye (frame number); used in debug log file", type=int)
+parser.add_argument("-v", "--eventnumber", help="%%v option from MotionEye; used in debug log file", type=int)
+args = parser.parse_args();
+
+def find_latest_file(args):
 	global FILE_FOUND, FILENAME
 	FILE_FOUND = 0
 	TODAYS_DATE = datetime.datetime.today().strftime('%Y-%m-%d')
-	TODAYS_DATE_FOLDER = cv.MEDIA_DIR + TODAYS_DATE + '/*.jpg'
+	TODAYS_DATE_FOLDER = args.mediadir + TODAYS_DATE + '/*.jpg'
 	logging.debug("Today's Date {}, Date Folder {}".format(TODAYS_DATE,TODAYS_DATE_FOLDER))
 
 	LIST_OF_FILES = glob.glob(TODAYS_DATE_FOLDER)
@@ -68,10 +81,10 @@ def find_latest_file():
 	if FILE_FOUND:
 		FILENAME = LATEST_FILE
 	else:
-		logging.debug("No File Found when searching")
+		logging.debug("No file found when searching")
 		FILENAME = './No_Image_Available_sm.jpg'
 
-def send_to_slack():
+def send_to_slack(args):
 	buf = cStringIO.StringIO()
 
 	c = pycurl.Curl()
@@ -82,10 +95,10 @@ def send_to_slack():
 
 	if os.path.isfile(FILENAME):
 		logging.debug("File found {}".format(FILENAME))
-                c.setopt(c.HTTPPOST, [("initial_comment", cv.INIT_COMMENT.format(cv.CAMERA_NAME) + MESSAGE_TIME),("title", cv.CAMERA_NAME),("channels", cv.CHANNEL_ID),("file", (c.FORM_FILE, FILENAME))])
+                c.setopt(c.HTTPPOST, [("initial_comment", cv.INIT_COMMENT.format(args.cameraname) + MESSAGE_TIME),("title", args.cameraname),("channels", cv.CHANNEL_ID),("file", (c.FORM_FILE, FILENAME))])
 	else:
 		logging.debug("File not found".format(FILENAME))
-		c.setopt(c.HTTPPOST, [("initial_comment", cv.INIT_COMMENT.format(cv.CAMERA_NAME) + MESSAGE_TIME),("channels", cv.CHANNEL_ID),])
+		c.setopt(c.HTTPPOST, [("initial_comment", cv.INIT_COMMENT + MESSAGE_TIME),("channels", cv.CHANNEL_ID),])
 
 	c.setopt(c.VERBOSE, cv.PYCURL_VERBOSE)
 
@@ -99,11 +112,13 @@ def send_to_slack():
 		logging.debug(f)
 
 logging.info("Motion detected on {}".format(cv.CAMERA_NAME))
+logging.debug("Frame number: {}".format(args.frame))
+logging.debug("Event number: {}".format(args.eventnumber))
 
 # retrieve the latest image and use in the webhook below, if configured in config.py
 if cv.DISPLAY_IMAGE:
-        logging.info("Finding last captured still image...")
-	find_latest_file()
+	logging.debug("Finding last captured still image...")
+	find_latest_file(args)
 
 # send motion notification to Slack
-send_to_slack()
+send_to_slack(args)
